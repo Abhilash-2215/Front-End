f1c4094a0111c6a82d0535103da755d8aa44b540
R Abhilash <abhilash81354@gmail.com>
2026-08-12 18:21:26 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,24 @@
+if ([]) {
+    console.log("hi ra sandeep");
+} else {
+    console.log("bye");
+}
+
+let day = 99;
+switch (day) {
+    case 1:
+        console.log("day 1");
+        break;
+    case 2:
+        console.log("day 1");
+        break;
+    case 3:
+        console.log("day 1");
+        break;
+    case 4:
+        console.log("day 1");
+        break;
+    default:
+        console.log("wrong value");
+        break;
+}
