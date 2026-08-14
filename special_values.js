5965bca1a538f3a16cdc48b487b97dea732aa9a4
R Abhilash <abhilash81354@gmail.com>
2026-08-14 18:58:59 +0530

    Add files via upload

---
+++ special_values.js
@@ -0,0 +1,21 @@
+let a2 = 10;
+let b2 = "Helllo World";
+console.log(a2);
+console.log(a2 + b2);
+
+let c2 = "234";
+let d2 = "342";
+let e = c2 * d2;
+console.log(e);
+
+let a3 = Symbol(1);
+let b3 = Symbol(1);
+console.log(a3 == b3);
+console.log(123 / false);
+
+let c3 = Infinity + true;
+console.log(c3);
+console.log(undefined + 1);
+console.log(1 / undefined);
+console.log(1343 * undefined);
+console.log(1343 - undefined);
