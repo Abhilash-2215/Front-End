76c54a2a254ca95d198a94ee1723fc9a94d43ca7
R Abhilash <abhilash81354@gmail.com>
2026-08-18 15:18:19 +0530

    Add files via upload

---
+++ settimeout.js
@@ -0,0 +1,10 @@
+// function show()
+// {
+//     console.log("ran after timeout");
+// }
+// setTimeout(show,2000);
+
+setTimeout(function show(){//no need function name
+    console.log("Ran after time out");
+
+},2000);
