51d4346f7afa77b60e23cdb2d6f65f48c171553e
R Abhilash <abhilash81354@gmail.com>
2026-08-21 19:13:26 +0530

    Add files via upload

---
+++ Promise.js
@@ -0,0 +1,9 @@
+const p = new Promise(resolve,reject)=>{
+
+    if(true){
+        resolve("this is my data,promise is fullfilled");
+    }
+    else{
+        reject("promise is not fullfilled");
+    }
+}
