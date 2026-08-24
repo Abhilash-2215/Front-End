dbaacae9d8116bbc64c5b4cad3572bbbc3936c03
R Abhilash <abhilash81354@gmail.com>
2026-08-24 20:42:38 +0530

    Add files via upload

---
+++ backtick.js
@@ -0,0 +1,11 @@
+let name = "Abhi";
+let age = 20;
+
+let message = `Hello, my name is ${name} and I am ${age} years old.`;
+
+let multiLine = `This is line one.
+This is line two.
+This is line three.`;
+
+console.log(message);
+console.log(multiLine);
