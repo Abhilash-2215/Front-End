baab8a5a733bb25a9386c05f731cdc6b1422238a
R Abhilash <abhilash81354@gmail.com>
2026-08-31 20:40:14 +0530

    Add files via upload

---
+++ arrows.js
@@ -0,0 +1,13 @@
+// const add = function(a,b){
+//     return a+b;
+// }
+const add=(a,b)=>{
+    return a+b ;
+}
+setTimeout(()=>{console.log("easy")},2000);
+const sq=(a)=>{
+    return a*a;
+};
+console.log(sq(5));
+
+
