bbb6c55b96e1100df47b66ad782084c19eb7e188
R Abhilash <abhilash81354@gmail.com>
2026-09-04 18:43:05 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,26 @@
+let arr=[1,2,3,4,5,6,8];
+//map-->always returns new array;
+// function fn(x){
+
+//     return x*10;
+// }                               //[undefined]
+let new_arr=arr.map(x=>x+10);
+console.log(new_arr);
+
+
+
+
+
+
+
+
+// function multiply_by_10(arr){
+//     let new_arr=[];
+//     for(let i=0;i<arr.length;i++){
+//         new_arr[i]=arr[i]*10;
+       
+//     }
+    
+// }
+// let new_arr=multiply_by_10(arr);
+// console.log(new_arr)
